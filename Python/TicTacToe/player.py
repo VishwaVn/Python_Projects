2ba482fdd7f5f76e274bb2ea70f7a1da97f115db
Vishnu VK <vishnukotemane005@gmail.com>
2024-07-07 16:13:28 +0530

    first commit

---
+++ Python/TicTacToe/player.py
@@ -0,0 +1,6 @@
+# import random and math 
+
+# create a generic class Called as player
+
+# create human player ineriting from the player ckass
+# create a ai player inheriting from the player class
